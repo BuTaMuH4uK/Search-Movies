49626be75e45cae5fa3f0be3ea25788a57d8ba23
Victor <butamuh4uk98@mail.ru>
2018-08-30 00:06:21 +0300

    1.1v

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './NotFound.css';
 
 const NotFound = () => (
-  <div>
+  <div className="notfound">
     <h3>We couldn't find what you were looking for :(</h3>
     <Link to={process.env.PUBLIC_URL + '/'}>Here you can browse the movies</Link>
   </div>
